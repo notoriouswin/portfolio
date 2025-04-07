b7ea535c0b5dda0a9ee0ce6e9d2452cae7e6dd40
Matheus Bessa 👨🏻‍💻 <bessadev13@gmail.com>
2025-04-07 00:34:25 -0300

    animação de x no menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,8 @@
 //Essa função ira fazer com que o menu abra mostrando a lista de navegação
-
 function menu() {
     const nav = document.getElementById("nav");
     nav.classList.toggle("active");
+
+    const hamburger = document.getElementById("hamburger")
+    hamburger.classList.toggle("hamburger-active")
 }
