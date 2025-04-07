a87abd14a2b979e8584bca04043bb94f77f69fbb
Matheus Bessa 👨🏻‍💻 <bessadev13@gmail.com>
2025-04-06 23:08:07 -0300

    teste

---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+//Essa função ira fazer com que o menu abra mostrando a lista de navegação
+
+function menu() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+}
