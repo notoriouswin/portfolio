fcab5c09b287ad1358cb6a185718c96b28bd2328
Matheus Bessa 👨🏻‍💻 <bessadev13@gmail.com>
2025-04-07 22:25:00 -0300

    update animation scroll

---
+++ src/js/scrollanim.js
@@ -0,0 +1,47 @@
+
+
+window.revelar = ScrollReveal({reset:true})
+
+// about
+
+revelar.reveal('.tAbout',{
+    duration: 1000,
+    distance: '200px',
+    origin: "left",
+    easing: "ease-in-out"
+})
+
+revelar.reveal('.profile-picture',{
+    duration: 1000,
+    distance: '300px',
+    origin: "right",
+    easing: "ease-in-out"
+})
+
+revelar.reveal('.text',{
+    duration: 1000,
+    distance: '100px',
+    origin: "bottom",
+    easing: "ease-in-out"
+})
+
+// project
+
+revelar.reveal('.tProject',{
+    duration: 1000,
+    distance: '250px',
+    origin: "left",
+    easing: "ease-in-out"
+})
+
+revelar.reveal('.project',{
+    duration: 1000,
+    distance: '250px',
+    origin: "left",
+    easing: "ease-in-out"
+})
+
+
+
+
+
